53f20fe36c5ac1f800f4333f76a6340ad740359b
kendomaniac <raffaele.calogero@gmail.com>
2019-02-27 06:46:41 +0100

    update

---
+++ R/verbalizza.R
@@ -0,0 +1,42 @@
+#' @title Una funzione che selezione gli studenti da verbalizzare.
+#' @description Una funzione che seleziona dal output di aggregaVotiModuli il set di studenti registrati per la verbalizzazione.
+#' @param excel.verbalizzazione, file excel della prenotazione per appello verbalizzante.
+#' @param voti.aggregati, file tab delimited con i voti aggregati generati da aggregaVotiModul.
+#' @param voti.verbalizzabili, tab delimited output con solo i voti aggregati per gli studenti registratiall'appello verbalizzante.
+#' @param studenti.missing, tab delimited output con la matrice del file di verbalizzazione per gli studenti che non trovo nei miei dati.
+#' @author Raffaele Calogero, raffaele.calogero [at] unito [dot] it, University of Torino, Italy
+#' @return  Il file tab delimited  riempito con i risultati parziali ed il voto integrato per gli studenti da verbalizzare.
+#'
+#' @examples
+#' \dontrun{
+#
+#' verbalizza(verbalizzazione="verbalizzante.xls", 
+#'                   voti.aggregati="canaleA_studenti_votiBBBMGU_aggregati_190227.txt",
+#'                   voti.verbalizzabili="canaleA_studenti_da_verbalizzare.txt", 
+#'                   studenti.missing="canaleA_studenti_da_verbalizzare_missing.txt")
+#' }
+#'
+#' @export
+
+verbalizza <- function(excel.verbalizzazione, voti.aggregati, voti.verbalizzabili, studenti.missing){
+  #carica dati da prenotazione verbalizzazione
+  bb <- loadWorkbook(excel.verbalizzazione)
+  bb.df <- readWorksheet(bb, sheet=1)
+  #identifica dove e' la matricola
+  matricola.col <- grep("Matricola", bb.df)
+  matricola.row <- which(bb.df[, matricola.col]=="Matricola")
+  matricola.esame <- as.numeric(bb.df[(matricola.row + 1):dim(bb.df)[1],matricola.col])
+  
+  #carica risultati di aggiungiRisultatiModulo che include i tre moduli
+  voti <- read.table(voti.aggregati, sep="\t", header=T, stringsAsFactors = F, quote = "\"")
+  voti.selezionati <- voti[which(as.numeric(voti$MATRICOLA)%in%matricola.esame),]
+  missing <- setdiff(matricola.esame, as.numeric(voti$MATRICOLA))
+  cat("\nStudenti da verbalizzare ma non nel file generato dalla funzione aggregaVotiModuli", length(missing),"\n")
+  if(length(missing) > 0){
+      bb.df <- bb.df[which(as.numeric(bb.df[(matricola.row + 1):dim(bb.df)[1],matricola.col])%in%missing),]
+      write.table(bb.df, studenti.missing, sep="\t", col.names = T, row.names=F)
+  }
+  #scrivi risultati del voto aggregato
+  write.table(voti.selezionati, voti.verbalizzabili, sep="\t", col.names = T, row.names=F)
+}
+
