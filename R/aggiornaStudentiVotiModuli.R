ad7feaf7adb541f356e7cd4496638ee2367c5e64
kendomaniac <raffaele.calogero@gmail.com>
2019-03-03 05:54:58 +0100

    update

--- R/aggiornaStudentiVotiModuli.R
+++ R/aggiornaStudentiVotiModuli.R
@@ -18,9 +18,38 @@
 #' @export
 
 aggiornaStudentiVotiModuli<- function(basiBiologiche, biologiaMolecolare, geneticaUmana){
-  all <- rbind(basiBiologiche,  biologiaMolecolare, geneticaUmana)
   
-  all <- all[!duplicated(all$MATRICOLA),]
-  cat("\nIl file studenti_voti.txt e' stato creato\n")
-  write.table(all, "studenti_voti.txt", sep="\t", col.names = T, row.names=F)
+  #defining function not equal
+  '%!in%' <- function(x,y)!('%in%'(x,y))
+  
+  all <- rbind(basiBiologiche,  biologiaMolecolare, geneticaUmana)
+  #senza duplicazioni
+  all.1 <- all[!duplicated(all$MATRICOLA),]
+  #i duplicati
+  all.2 <- all[duplicated(all$MATRICOLA),]
+  #controllo quali elementi hanno valori gia' assegnati
+  some.nona <- apply(all.2, 1,function(x){length(!is.na(x))})
+  all.2 <- all.2[which(some.nona > 0),]
+  #identifico i duplicati con i voti bel file senza duplicati
+  all.3 <- all.1[which(all.1$MATRICOLA %in%all.2$MATRICOLA),]
+  #controllo quali elementi hanno vaoli assegnati
+  some.nona <- apply(all.3, 1,function(x){length(!is.na(x))})
+  #identifico i non duplicati con valori assegnati
+  all.3 <- all.3[which(some.nona > 0),]
+  #non dovrebbero esserci sovrapposizioni tra all.2. ed all.3
+  critical <- intersect(all.2$MATRICOLA, all.3$MATRICOLA)
+  
+  if(length(critical) == 0){
+    all.0 <- all.1[which(all.$MATRICOLA %!in% c(all.2$MATRICOLA, all.3$MATRICOLA)),]
+    all.final <- rbindi(all.0, all.2, all.3)
+    
+    cat("\nIl file studenti_voti.txt e' stato creato\n")
+    write.table(all.final, "studenti_voti.txt", sep="\t", col.names = T, row.names=F)
+    return(0)
+  }else{
+    cat("\nATTENZIONE: ci sono dei duplicati con dei voti! Non dovrebbe succedere.\n Controllare i files all2duplicati.txt ed all3noduplicati.txt\n")
+    write.table(all.2, "all2duplicati.txt", sep="\t", col.names = T, row.names=F)
+    write.table(all.3, "all3noduplicati.txt", sep="\t", col.names = T, row.names=F)
+    return(1)
+  }
 }
