b2ac4d918e04e6c2890e967b1fd5db71d3061afd
kendomaniac <raffaele.calogero@gmail.com>
2019-03-04 06:06:24 +0100

    ritornato indietro alla configurazione con vosti.studenti vuoto. aggiungo adesso alla fine una funzione di integrazione di due file aggregati

--- R/aggiornaStudentiVotiModuli.R
+++ R/aggiornaStudentiVotiModuli.R
@@ -25,41 +25,9 @@ aggiornaStudentiVotiModuli<- function(basiBiologiche, biologiaMolecolare, geneti
   all <- rbind(basiBiologiche,  biologiaMolecolare, geneticaUmana)
   #senza duplicazioni
   all.1 <- all[!duplicated(all$MATRICOLA),]
-  #i duplicati
-  all.2 <- all[duplicated(all$MATRICOLA),]
-  #controllo quali elementi hanno valori gia' assegnati
-  some.nona <- apply(all.2, 1,function(x){
-    sum(!is.na(x[7:13]))  
-           })
-  all.2 <- all.2[which(some.nona > 0),]
-  #identifico i duplicati con i voti bel file senza duplicati
-  all.3 <- all.1[which(all.1$MATRICOLA %in% all.2$MATRICOLA),]
-  #controllo quali elementi hanno valori assegnati
-  some.nona <- apply(all.3, 1,function(x){
-    sum(!is.na(x[7:13]))
-           })
-  #identifico i non duplicati con valori assegnati
-  all.3 <- all.3[which(some.nona > 0),]
-  #non dovrebbero esserci sovrapposizioni tra all.2. ed all.3
-  critical <- intersect(all.2$MATRICOLA, all.3$MATRICOLA)
-  
-  if(length(critical) == 0){
-    all.0 <- all.1[which(all.1$MATRICOLA %!in% c(all.2$MATRICOLA, all.3$MATRICOLA)),]
-    all.final <- rbind(all.0, all.2, all.3)
-    
-    cat("\nIl file studenti_voti.txt e' stato creato\n")
-    write.table(all.final, "studenti_voti.txt", sep="\t", col.names = T, row.names=F)
-    return(0)
-  }else{
-    cat("\nATTENZIONE: ci sono dei duplicati con dei voti! Non dovrebbe succedere.
-        \nControllare i files all2duplicati.txt ed all3noduplicati.txt\n")
-    write.table(all.2, "all2duplicati.txt", sep="\t", col.names = T, row.names=F)
-    write.table(all.3, "all3noduplicati.txt", sep="\t", col.names = T, row.names=F)
-    
-    all.0 <- all.1[which(all.$MATRICOLA %!in% c(all.2$MATRICOLA, all.3$MATRICOLA)),]
-    all.final <- rbind(all.0, all.2, all.3)
-    cat("\nIl file studenti_voti.txt e' stato creato ma ci sono problemi nei duplicati: controllare!\n")
-    write.table(all.final, "studenti_voti.txt", sep="\t", col.names = T, row.names=F)
-    return(1)
-  }
+     
+  cat("\nIl file studenti_voti.txt e' stato creato\n")
+  write.table(all.final, "studenti_voti.txt", sep="\t", col.names = T, row.names=F)
+  return(0)
+
 }
